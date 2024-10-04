bb8f1d80f4b33ece70603185443d6913c577d08b
sagarrpatil <66763299+sagarrpatil@users.noreply.github.com>
2024-10-04 16:27:29 +0530

    Add files via upload

---
+++ dist/Button.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var Button = function Button(_ref) {
+  var label = _ref.label,
+    onClick = _ref.onClick;
+  return /*#__PURE__*/_react["default"].createElement("button", {
+    onClick: onClick
+  }, label);
+};
+var _default = exports["default"] = Button;
