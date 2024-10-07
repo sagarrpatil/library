dec352b061d5c32d7b780b6a83748a6b001909b8
Sagar Patil <sagarp@perscitussln.com>
2024-10-07 12:50:06 +0530

    test

--- dist/Button.js
+++ dist/Button.js
@@ -1,19 +1,18 @@
 "use strict";
 
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = BasicButtonGroup;
+exports.default = BasicButtonGroup;
 var React = _interopRequireWildcard(require("react"));
 var _Button = _interopRequireDefault(require("@mui/material/Button"));
 var _ButtonGroup = _interopRequireDefault(require("@mui/material/ButtonGroup"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(e) { return e ? t : r; })(e); }
-function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != _typeof(e) && "function" != typeof e) return { "default": e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n["default"] = e, t && t.set(e, n), n; }
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
+function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
 function BasicButtonGroup() {
-  return /*#__PURE__*/React.createElement(_ButtonGroup["default"], {
+  return /*#__PURE__*/React.createElement(_ButtonGroup.default, {
     variant: "contained",
     "aria-label": "Basic button group"
-  }, /*#__PURE__*/React.createElement(_Button["default"], null, "One"), /*#__PURE__*/React.createElement(_Button["default"], null, "Two"), /*#__PURE__*/React.createElement(_Button["default"], null, "Three"));
+  }, /*#__PURE__*/React.createElement(_Button.default, null, "One"), /*#__PURE__*/React.createElement(_Button.default, null, "Two"), /*#__PURE__*/React.createElement(_Button.default, null, "Three"));
 }
--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,18 @@ Object.defineProperty(exports, "__esModule", {
 });
 Object.defineProperty(exports, "Button", {
   enumerable: true,
-  get: function get() {
-    return _Button["default"];
+  get: function () {
+    return _Button.default;
   }
 });
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
 var _Button = _interopRequireDefault(require("./Button"));
-function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+const App = () => {
+  return /*#__PURE__*/_react.default.createElement("div", null, "Hello", /*#__PURE__*/_react.default.createElement(_Button.default, {
+    label: "Click Me!",
+    onClick: () => alert('Button Clicked!')
+  }));
+};
+_reactDom.default.render(/*#__PURE__*/_react.default.createElement(App, null), document.getElementById('root'));
